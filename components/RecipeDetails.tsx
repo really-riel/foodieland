8d0e86445097e63f5d4501acde2cf442f859169c
really-riel <110791013+really-riel@users.noreply.github.com>
2024-08-11 10:45:40 +0100

    added Playicon

--- components/RecipeDetails.tsx
+++ components/RecipeDetails.tsx
@@ -2,11 +2,13 @@ import { RecipeCardProps } from "@/types";
 import { NutritionInfo, cookDatas } from "@/utils/data";
 import Image from "next/image";
 import React from "react";
-import { BiStopwatch } from "react-icons/bi";
+
 import { BsFillStopwatchFill } from "react-icons/bs";
 import { FiPrinter } from "react-icons/fi";
 import { LuShare } from "react-icons/lu";
 import { PiForkKnifeFill } from "react-icons/pi";
+import play from "../public/play.png";
+import playIcon from "../public/playIcon.png";
 
 interface RecipeDetailsProps {
   recipe: RecipeCardProps;
@@ -64,8 +66,18 @@ function RecipeDetails({ recipe }: RecipeDetailsProps) {
       </div>
       <div className=""></div>
       <div className="grid lg:grid-cols-2 gap-6">
-        <div className="">
-          <Image alt={recipe.title} src={recipe.pic} />
+        <div className="relative w-full">
+          <Image className="w-full" alt={recipe.title} src={recipe.pic} />
+          <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]  ">
+            <div className="relative grid place-content-center">
+              <Image src={play} alt="play" />
+              <Image
+                className="absolute top-[50%] bottom-[50%] left-[50%] right-[50%] -translate-x-[50%] -translate-y-[50%]"
+                src={playIcon}
+                alt="play"
+              />
+            </div>
+          </div>
         </div>
         <div className=" bg-foodie px-2 py-5 rounded-2xl">
           <h2 className="font-bold mb-3">Nutrition information</h2>
