7a205ab19ecec3b56eeb581f4a6f9801b64c9c70
really-riel <110791013+really-riel@users.noreply.github.com>
2024-08-11 17:15:28 +0100

    Update Directions.tsx

--- components/Directions.tsx
+++ components/Directions.tsx
@@ -10,62 +10,64 @@ function Directions() {
     <section className="flex flex-col gap-7">
       <h1 className="font-bold text-foodieLg mb-10">Directions</h1>
 
-      <div className=" flex gap-4">
-        <div className="">
-          <Checkbox
-            icon={<RadioButtonUncheckedIcon />}
-            checkedIcon={<CheckCircleIcon />}
-          />
+      <div className="lg:w-[62%]">
+        <div className=" flex  gap-4">
+          <div className="">
+            <Checkbox
+              icon={<RadioButtonUncheckedIcon />}
+              checkedIcon={<CheckCircleIcon />}
+            />
+          </div>
+          <div className="flex flex-col gap-8">
+            <p className="font-bold text-2xl">1. Lorem ipsum dolor sit amet</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              corporis iure suscipit tempora ab, dolorum praesentium ratione
+              vel, quisquam delectus quidem, rem et voluptates autem omnis eaque
+              unde placeat voluptatibus.
+            </p>
+            <Image alt="cooking" src={direction} className="w-full" />
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+              corporis similique quasi eaque recusandae ad. Necessitatibus,
+              fugiat, corporis natus cumque eaque magni obcaecati nam illo
+              nostrum a aliquid. Ipsum, excepturi.
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-8">
-          <p className="font-bold text-2xl">1. Lorem ipsum dolor sit amet</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-            corporis iure suscipit tempora ab, dolorum praesentium ratione vel,
-            quisquam delectus quidem, rem et voluptates autem omnis eaque unde
-            placeat voluptatibus.
-          </p>
-          <Image alt="cooking" src={direction} className="w-full" />
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
-            corporis similique quasi eaque recusandae ad. Necessitatibus,
-            fugiat, corporis natus cumque eaque magni obcaecati nam illo nostrum
-            a aliquid. Ipsum, excepturi.
-          </p>
+        <div className="flex gap-4">
+          <div className="">
+            <Checkbox
+              icon={<RadioButtonUncheckedIcon />}
+              checkedIcon={<CheckCircleIcon />}
+            />
+          </div>
+          <div className="flex flex-col gap-8">
+            <p className="font-bold text-2xl">2. Lorem ipsum dolor sit amet</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quos
+              corporis quidem magnam ipsum delectus ea ducimus, suscipit minima
+              nisi autem repellat nostrum at eligendi expedita unde ab nihil
+              voluptas.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex gap-4">
-        <div className="">
-          <Checkbox
-            icon={<RadioButtonUncheckedIcon />}
-            checkedIcon={<CheckCircleIcon />}
-          />
-        </div>
-        <div className="flex flex-col gap-8">
-          <p className="font-bold text-2xl">2. Lorem ipsum dolor sit amet</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, quos
-            corporis quidem magnam ipsum delectus ea ducimus, suscipit minima
-            nisi autem repellat nostrum at eligendi expedita unde ab nihil
-            voluptas.
-          </p>
-        </div>
-      </div>
-      <div className="flex gap-4">
-        <div className="">
-          <Checkbox
-            icon={<RadioButtonUncheckedIcon />}
-            checkedIcon={<CheckCircleIcon />}
-          />
-        </div>
-        <div className="flex flex-col gap-8">
-          <p className="font-bold text-2xl">3. Lorem ipsum dolor sit amet</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-            dolor autem commodi earum minima sint iste doloremque est molestias
-            cum, vero quisquam reiciendis officiis eius perferendis assumenda
-            architecto, necessitatibus inventore.
-          </p>
+        <div className="flex gap-4">
+          <div className="">
+            <Checkbox
+              icon={<RadioButtonUncheckedIcon />}
+              checkedIcon={<CheckCircleIcon />}
+            />
+          </div>
+          <div className="flex flex-col gap-8">
+            <p className="font-bold text-2xl">3. Lorem ipsum dolor sit amet</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus dolor autem commodi earum minima sint iste doloremque
+              est molestias cum, vero quisquam reiciendis officiis eius
+              perferendis assumenda architecto, necessitatibus inventore.
+            </p>
+          </div>
         </div>
       </div>
     </section>
